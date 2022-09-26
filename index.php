5ee5ffb482fa14e2fc9e24bc3bcb0d5938fe9572
DaniS Almata <dsole6@xtec.cat>
2022-09-26 12:07:47 +0200

    omplert arxius varis

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
-<html>
-<FRAMESET cols="220,*">
-
-<frame name="sx" src="sx.htm">
-<frame name="central" src="inicio.htm">
-
-</FRAMESET>
-</html>
-
-
+<?php
+require __DIR__ . "/inc/bootstrap.php";
+ 
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = explode( '/', $uri );
+ 
+if ((isset($uri[2]) && $uri[2] != 'user') || !isset($uri[3])) {
+    header("HTTP/1.1 404 Not Found");
+    exit();
+}
+ 
+require PROJECT_ROOT_PATH . "/Controller/Api/UserController.php";
+ 
+$objFeedController = new UserController();
+$strMethodName = $uri[3] . 'Action';
+$objFeedController->{$strMethodName}();
+?>
 
