ae85325cb06138ddc434a51df09a0943e4c3cb6a
DaniS Almata <dsole6@xtec.cat>
2022-09-28 13:00:02 +0200

    l'api funciona, next step ampliar api i login

--- class/mysqlclase.php
+++ class/mysqlclase.php
@@ -2,16 +2,31 @@
 
 function Conectarse(){
 
- include ('config.cfg');
- if (!($link=mysql_connect(SERVER,USER,PASSWORD))){
+ // needs to include ('../inc/config.cfg');
+ 
+ try {
+   $connection = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE_NAME);
+   if ( mysqli_connect_errno()) {
+       throw new Exception("Could not connect to database.");   
+   }
+   } catch (Exception $e) {
+      throw new Exception($e->getMessage());   
+   }
+return $connection;}        
+ 
+ 
+ 
+ /*
+ if (!($link=mysql_connect(DB_HOST,DB_USERNAME,DB_PASSWORD))){
     echo "Error conectando a la base de datos.";
     exit();  }
-   if (!mysql_select_db(BBDD,$link))
+   if (!mysql_select_db(DB_DATABASE_NAME,$link))
    {      echo "Error seleccionando la base de datos.";
    exit();   }   
-   return $link;}
+   return $link;}*/
 
 //function CerrarConexion(){
 //	mysql_close($link);
 // }
+
 ?>
--- escandallapi.php
+++ escandallapi.php
@@ -1,21 +1,43 @@
+
+
 <?php
+header("Content-Type:application/json");
 
-include ('mysqlclase.php');
-include ('login.php');
+//controlar que l'usuari és correcte
+//sino redirecció a la web de login
+if (SESSION)
 
-// tutorial https://code.tutsplus.com/tutorials/how-to-build-a-simple-rest-api-in-php--cms-37000
-// a revisar crear i enviar un json
-$url = 'https://domain.com/folder/post_data.php';
-$data = json_decode(file_get_contents("https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations"), true);
-$crl = curl_init($url);
-curl_setopt($crl, CURLOPT_POST, 1);
-curl_setopt($crl, CURLOPT_POSTFIELDS, json_encode($data));
-curl_setopt($crl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-curl_setopt($crl, CURLOPT_RETURNTRANSFER, 1);
-$result = curl_exec($crl);
-echo $result;
-//fi a revisar crear i enviar un json
+//include ('login.php');
 
+if (isset($_GET['order_id']) && $_GET['order_id']!="") {
+    include ('inc/config.php');
+	include ('class/mysqlclase.php');
+	$order_id = $_GET['order_id'];
+    $con = Conectarse();
+	$result = mysqli_query(
+	$con,
+	"SELECT * FROM `e_materies`");
+	if(mysqli_num_rows($result)>0){
+	$row = mysqli_fetch_array($result);
+	$amount = $row['Materia'];
+	$response_code = $row['Umesura'];
+	$response_desc = $row['Preu'];
+	response($order_id, $amount, $response_code,$response_desc);
+	mysqli_close($con);
+	}else{
+		response(NULL, NULL, 200,"No Record Found");
+		}
+}else{
+	response(NULL, NULL, 400,"Invalid Request");
+	}
 
-
+function response($order_id,$amount,$response_code,$response_desc){
+	$response['order_id'] = $order_id;
+	$response['amount'] = $amount;
+	$response['response_code'] = $response_code;
+	$response['response_desc'] = $response_desc;
+	
+	$json_response = json_encode($response);
+	echo $json_response;
+}
 ?>
